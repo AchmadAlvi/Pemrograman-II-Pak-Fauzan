badeb44d7265a2136f5dc28338d1a87eb24d72f5
AchmadAlvi <145420111+AchmadAlvi@users.noreply.github.com>
2023-10-20 08:17:59 +0700

    Pertemuan 04

---
+++ Pertemuan4_PraktikBangunDatar/Lingkaran.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan4_PraktikBangunDatar;
+
+/**
+ *
+ * @author mmccom
+ */
+public class Lingkaran extends BangunDatar {
+    // jari-jari lingkaran
+    float r;
+}

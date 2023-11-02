b6e03c397aceeeb8ecc8a883286f63e44c57f622
AchmadAlvi <145420111+AchmadAlvi@users.noreply.github.com>
2023-11-02 12:57:17 +0700

    Pertemuan 06

---
+++ Pertemuan_6_Enkapsulasi/AkunBank.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan_6_Enkapsulasi;
+
+/**
+ *
+ * @author mmccom
+ */
+public class AkunBank {
+    private String NoRek;
+    private String Pemilik;
+    private int Saldo;
+    
+    public void setNoRek(String NoRek){
+         this.NoRek = NoRek;
+    }
+    
+    public void setPemilik(String Pemilik){
+        this.Pemilik = Pemilik; 
+    }
+    
+    public void setSaldo (int Saldo){
+        this.Saldo = Saldo;
+    }
+      
+    public String getNoRek(){
+        return NoRek;
+    }
+    
+    public String getPemilik(){
+        return Pemilik;
+    }
+    public int getSaldo(){
+        return Saldo;
+    }
+    
+    public void deposit(int nominal){
+        if(nominal > 0){
+            Saldo += nominal;
+        }
+    }
+    
+    public void TarikUang(int nominal){
+        if(nominal > 0 && nominal <= Saldo){
+            Saldo -= nominal;
+        }
+    }
+}
